4fd4dd95aa295ec0ca6dcb9e2e926a3d63602c26
ankit_ankit <ankit15199t@gmail.com>
2025-11-18 19:47:59 -0800

    Add Sorter skeleton

---
+++ src/Sorter.java
@@ -0,0 +1,17 @@
+public class Sorter {
+
+    public static void main(String[] args) {
+        // TODO: we'll fill this in later
+        System.out.println("Git exercise demo");
+    }
+
+    public int[] sortInts(int[] nums) {
+        // TODO: feature 1
+        return nums;
+    }
+
+    public String[] sortStrings(String[] words) {
+        // TODO: feature 2
+        return words;
+    }
+}
